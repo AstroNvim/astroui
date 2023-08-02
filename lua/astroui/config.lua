9bd1ccd30a738734dfbd49b2319f20a679b27589
Micah Halter <micah@mehalter.com>
2023-08-02 09:01:39 -0400

    docs: start adding configuration documentation

--- lua/astroui/config.lua
+++ lua/astroui/config.lua
@@ -1,6 +1,41 @@
+--- ### AstroNvim UI Configuration
+--
+-- This module simply defines the configuration table structure for `opts` used in:
+--
+--    require("astroui").setup(opts)
+--
+-- @module astroui.config
+-- @copyright 2023
+-- @license GNU General Public License v3.0
+
 return {
+  --- Colorscheme set on startup
+  -- @usage colorscheme = "astrodark"
   colorscheme = nil,
+  --- Override highlights in any colorscheme
+  -- @field init table of highlights to apply to all colorschemes
+  -- @field colorscheme_name override highlights in the colorscheme with name: `colorscheme_name`
+  -- @usage highlights = {
+  --      -- this table overrides highlights in all colorschemes
+  --      init = {
+  --        Normal = { bg = "#000000" },
+  --      },
+  --      -- a table of overrides/changes when applying astrotheme
+  --      astrotheme = {
+  --        Normal = { bg = "#000000" },
+  --      },
+  -- }
   highlights = {},
+  --- A table of icons in the UI using NERD fonts
+  -- @usage icons = {
+  --   GitAdd = "",
+  -- }
+  icons = {},
+  --- A table of only text "icons" used when icons are disabled
+  -- @usage text_icons = {
+  --   GitAdd = "[+]",
+  -- }
+  text_icons = {},
   status = {
     attributes = {},
     buf_matchers = {},
@@ -12,6 +47,4 @@ return {
     setup_colors = nil,
     sign_handlers = {},
   },
-  icons = {},
-  text_icons = {},
 }
