dc224f1b2596ba5e1888cb87b2870a6cbbba2ab5
Micah Halter <micah@mehalter.com>
2024-07-19 13:37:01 -0400

    fix(status): update `conform` integration to use non-deprecated functions

--- lua/astroui/status/provider.lua
+++ lua/astroui/status/provider.lua
@@ -527,10 +527,10 @@ function M.lsp_client_names(opts)
       end
     end
     if opts.integrations.conform and package.loaded["conform"] then -- conform integration
-      local conform = require "conform"
-      if not conform.will_fallback_lsp { bufnr = bufnr } then
-        vim.list_extend(buf_client_names, vim.tbl_map(function(c) return c.name end, conform.list_formatters(bufnr)))
-      end
+      vim.list_extend(
+        buf_client_names,
+        vim.tbl_map(function(c) return c.name end, require("conform").list_formatters_to_run(bufnr))
+      )
     end
     if opts.integrations["nvim-lint"] and package.loaded["lint"] then -- nvim-lint integration
       vim.list_extend(buf_client_names, require("lint")._resolve_linter_by_ft(vim.bo[bufnr].filetype))
