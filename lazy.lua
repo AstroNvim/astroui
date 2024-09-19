3d354f2e496fc2a23bfc2082caa6f516da19bae0
Micah Halter <micah@mehalter.com>
2024-09-19 07:57:18 -0400

    ci: add `lazy.lua` for configuring `opts_extend`

---
+++ lazy.lua
@@ -0,0 +1,11 @@
+return {
+  "AstroNvim/astroui",
+  opts_extend = {
+    "status.winbar.enabled.filetype",
+    "status.winbar.enabled.buftype",
+    "status.winbar.enabled.bufname",
+    "status.winbar.disabled.filetype",
+    "status.winbar.disabled.buftype",
+    "status.winbar.disabled.bufname",
+  },
+}
