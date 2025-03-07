5c8e2c2a911022a2232a1efdef3b2da1ac3b75e1
Micah Halter <micah@mehalter.com>
2025-03-07 09:28:31 -0500

    feat(status): allow configuration of default status component options through the setup function

--- lua/astroui/config.lua
+++ lua/astroui/config.lua
@@ -75,6 +75,17 @@
 ---}
 ---```
 ---@field colors (StringMap|(fun(colors:StringMap):StringMap?))?
+---Set up the default configuration options for the provided statusline components.
+---TODO: this field still needs to add strong typing
+---Example:
+---```lua
+---components = {
+---  file_info = {
+---    file_read_only = false
+---  }
+---}
+---```
+---@field components table?
 ---**MEANT FOR INTERNAL USE ONLY**
 ---A table of fallback colors if a colorscheme used by the user does not have a highlight group, the entry point to this are typically through the `status.colors` option.
 ---@field fallback_colors StringMap?
@@ -112,7 +123,7 @@
 ---TODO: this field still needs to add strong typing
 ---Example:
 ---```lua
----provider = {
+---providers = {
 ---  lsp_client_names = {
 ---    mappings = {
 ---      ruff_organize_imports = "ruff"
@@ -257,6 +268,331 @@ local M = {
   status = {
     attributes = {},
     colors = {},
+    components = {
+      file_info = {
+        file_icon = {
+          hl = function(self) return require("astroui.status.hl").file_icon "statusline"(self) end,
+          padding = { left = 1, right = 1 },
+        },
+        filename = false,
+        filetype = {},
+        file_modified = {
+          padding = { left = 1 },
+          condition = function(...) return require("astroui.status.condition").is_file(...) end,
+        },
+        file_read_only = {
+          padding = { left = 1 },
+          condition = function(...) return require("astroui.status.condition").is_file(...) end,
+        },
+        surround = {
+          separator = "left",
+          color = "file_info_bg",
+          condition = function(...) return require("astroui.status.condition").has_filetype(...) end,
+        },
+        hl = function() return require("astroui.status.hl").get_attributes "file_info" end,
+      },
+      tabline_file_info = {
+        file_icon = {
+          condition = function(self) return not self._show_picker end,
+          hl = function(self) return require("astroui.status.hl").file_icon "tabline"(self) end,
+        },
+        filename = {},
+        filetype = false,
+        unique_path = {
+          hl = function(self) return require("astroui.status.hl").get_attributes(self.tab_type .. "_path") end,
+        },
+        close_button = {
+          hl = function(self) return require("astroui.status.hl").get_attributes(self.tab_type .. "_close") end,
+          padding = { left = 1, right = 1 },
+          on_click = {
+            callback = function(_, minwid) require("astrocore.buffer").close(minwid) end,
+            minwid = function(self) return self.bufnr end,
+            name = "heirline_tabline_close_buffer_callback",
+          },
+        },
+        padding = { right = 1 },
+        hl = function(self)
+          local tab_type = self.tab_type
+          if self._show_picker and self.tab_type ~= "buffer_active" then tab_type = "buffer_visible" end
+          return require("astroui.status.hl").get_attributes(tab_type)
+        end,
+        surround = false,
+      },
+      nav = {
+        ruler = {},
+        percentage = { padding = { left = 1 } },
+        scrollbar = { padding = { left = 1 }, hl = { fg = "scrollbar" } },
+        surround = { separator = "right", color = "nav_bg" },
+        hl = function() return require("astroui.status.hl").get_attributes "nav" end,
+        update = { "CursorMoved", "CursorMovedI", "BufEnter" },
+      },
+      cmd_info = {
+        macro_recording = {
+          icon = { kind = "MacroRecording", padding = { right = 1 } },
+          condition = function() return require("astroui.status.condition").is_macro_recording() end,
+          update = { "RecordingEnter", "RecordingLeave" },
+        },
+        search_count = {
+          icon = { kind = "Search", padding = { right = 1 } },
+          padding = { left = 1 },
+          condition = function() return require("astroui.status.condition").is_hlsearch() end,
+        },
+        showcmd = {
+          padding = { left = 1 },
+          condition = function() return require("astroui.status.condition").is_statusline_showcmd() end,
+        },
+        surround = {
+          separator = "center",
+          color = "cmd_info_bg",
+          condition = function()
+            local condition = require "astroui.status.condition"
+            return condition.is_hlsearch() or condition.is_macro_recording() or condition.is_statusline_showcmd()
+          end,
+        },
+        condition = function() return vim.opt.cmdheight:get() == 0 end,
+        hl = function() return require("astroui.status.hl").get_attributes "cmd_info" end,
+      },
+      mode = {
+        mode_text = false,
+        paste = false,
+        spell = false,
+        surround = {
+          separator = "left",
+          color = function() return require("astroui.status.hl").mode_bg() end,
+          update = { "ModeChanged", pattern = "*:*" },
+        },
+        hl = function() return require("astroui.status.hl").get_attributes "mode" end,
+        update = {
+          "ModeChanged",
+          pattern = "*:*",
+          callback = function() vim.schedule(vim.cmd.redrawstatus) end,
+        },
+      },
+      breadcrumbs = {
+        padding = { left = 1 },
+        condition = function() return require("astroui.status.condition").aerial_available() end,
+        update = "CursorMoved",
+      },
+      separated_path = { padding = { left = 1 }, update = { "BufEnter", "DirChanged" } },
+      git_branch = {
+        git_branch = { icon = { kind = "GitBranch", padding = { right = 1 } } },
+        surround = {
+          separator = "left",
+          color = "git_branch_bg",
+          condition = function(...) return require("astroui.status.condition").is_git_repo(...) end,
+        },
+        hl = function() return require("astroui.status.hl").get_attributes "git_branch" end,
+        on_click = {
+          name = "heirline_branch",
+          callback = function()
+            local fzf_lua_avail, fzf_lua = pcall(require, "fzf-lua")
+            if fzf_lua_avail then
+              fzf_lua.git_branches()
+              return
+            end
+
+            local telescope_avail, telescope_builtin = pcall(require, "telescope.builtin")
+            if telescope_avail then
+              telescope_builtin.git_branches { use_file_path = true }
+              return
+            end
+
+            local snacks_avail, snacks = pcall(require, "snacks")
+            if snacks_avail then
+              snacks.picker.git_branches()
+              return
+            end
+          end,
+        },
+        update = {
+          "User",
+          pattern = { "GitSignsUpdate", "GitSignsChanged" },
+          callback = function() vim.schedule(vim.cmd.redrawstatus) end,
+        },
+        init = function(...) return require("astroui.status.init").update_events { "BufEnter" }(...) end,
+      },
+      git_diff = {
+        added = { icon = { kind = "GitAdd", padding = { left = 1, right = 1 } } },
+        changed = { icon = { kind = "GitChange", padding = { left = 1, right = 1 } } },
+        removed = { icon = { kind = "GitDelete", padding = { left = 1, right = 1 } } },
+        hl = function() return require("astroui.status.hl").get_attributes "git_diff" end,
+        on_click = {
+          name = "heirline_git",
+          callback = function()
+            local fzf_lua_avail, fzf_lua = pcall(require, "fzf-lua")
+            if fzf_lua_avail then
+              fzf_lua.git_status()
+              return
+            end
+
+            local telescope_avail, telescope_builtin = pcall(require, "telescope.builtin")
+            if telescope_avail then
+              telescope_builtin.git_status { use_file_path = true }
+              return
+            end
+
+            local snacks_avail, snacks = pcall(require, "snacks")
+            if snacks_avail then
+              snacks.picker.git_status()
+              return
+            end
+          end,
+        },
+        surround = {
+          separator = "left",
+          color = "git_diff_bg",
+          condition = function(...) return require("astroui.status.condition").git_changed(...) end,
+        },
+        update = {
+          "User",
+          pattern = { "GitSignsUpdate", "GitSignsChanged", "MiniDiffUpdated" },
+          callback = function() vim.schedule(vim.cmd.redrawstatus) end,
+        },
+        init = function(...) return require("astroui.status.init").update_events { "BufEnter" }(...) end,
+      },
+      diagnostics = {
+        ERROR = { icon = { kind = "DiagnosticError", padding = { left = 1, right = 1 } } },
+        WARN = { icon = { kind = "DiagnosticWarn", padding = { left = 1, right = 1 } } },
+        INFO = { icon = { kind = "DiagnosticInfo", padding = { left = 1, right = 1 } } },
+        HINT = { icon = { kind = "DiagnosticHint", padding = { left = 1, right = 1 } } },
+        surround = {
+          separator = "left",
+          color = "diagnostics_bg",
+          condition = function(...) return require("astroui.status.condition").has_diagnostics(...) end,
+        },
+        hl = function() return require("astroui.status.hl").get_attributes "diagnostics" end,
+        on_click = {
+          name = "heirline_diagnostic",
+          callback = function()
+            local fzf_lua_avail, fzf_lua = pcall(require, "fzf-lua")
+            if fzf_lua_avail then
+              fzf_lua.diagnostics_document()
+              return
+            end
+
+            local telescope_avail, telescope_builtin = pcall(require, "telescope.builtin")
+            if telescope_avail then
+              telescope_builtin.diagnostics { bufnr = 0 }
+              return
+            end
+
+            local snacks_avail, snacks = pcall(require, "snacks")
+            if snacks_avail then
+              snacks.picker.diagnostics_buffer()
+              return
+            end
+          end,
+        },
+        update = { "DiagnosticChanged", "BufEnter" },
+      },
+      treesitter = {
+        str = { str = "TS", icon = { kind = "ActiveTS", padding = { right = 1 } } },
+        surround = {
+          separator = "right",
+          color = "treesitter_bg",
+          condition = function(...) return require("astroui.status.condition").treesitter_available(...) end,
+        },
+        hl = function() return require("astroui.status.hl").get_attributes "treesitter" end,
+        update = { "OptionSet", pattern = "syntax" },
+        init = function(...) return require("astroui.status.init").update_events { "BufEnter" }(...) end,
+      },
+      lsp = {
+        lsp_progress = {
+          str = "",
+          padding = { right = 1 },
+          update = {
+            "User",
+            pattern = "AstroLspProgress",
+            callback = function() vim.schedule(vim.cmd.redrawstatus) end,
+          },
+        },
+        lsp_client_names = {
+          str = "LSP",
+          update = {
+            "LspAttach",
+            "LspDetach",
+            "BufEnter",
+            "FileType",
+            "VimResized",
+            callback = function() vim.schedule(vim.cmd.redrawstatus) end,
+          },
+          icon = { kind = "ActiveLSP", padding = { right = 2 } },
+        },
+        hl = function() return require("astroui.status.hl").get_attributes "lsp" end,
+        surround = {
+          separator = "right",
+          color = "lsp_bg",
+          condition = function(...) return require("astroui.status.condition").lsp_attached(...) end,
+        },
+        on_click = {
+          name = "heirline_lsp",
+          callback = function() vim.schedule(vim.cmd.LspInfo) end,
+        },
+      },
+      virtual_env = {
+        virtual_env = { icon = { kind = "Environment", padding = { right = 1 } } },
+        surround = {
+          separator = "right",
+          color = "virtual_env_bg",
+          condition = function() return require("astroui.status.condition").has_virtual_env() end,
+        },
+        hl = function() return require("astroui.status.hl").get_attributes "virtual_env" end,
+        on_click = {
+          name = "heirline_virtual_env",
+          callback = function()
+            if vim.fn.exists ":VenvSelect" > 0 then vim.schedule(vim.cmd.VenvSelect) end
+          end,
+        },
+      },
+      foldcolumn = {
+        foldcolumn = { padding = { right = 1 } },
+        condition = function() return require("astroui.status.condition").foldcolumn_enabled() end,
+        on_click = {
+          name = "fold_click",
+          callback = function(...)
+            local get_icon = require("astroui").get_icon
+            local char = require("astroui.status.utils").statuscolumn_clickargs(...).char
+            local fillchars = vim.opt_local.fillchars:get()
+            if char == (fillchars.foldopen or get_icon "FoldOpened") then
+              vim.cmd "norm! zc"
+            elseif char == (fillchars.foldclose or get_icon "FoldClosed") then
+              vim.cmd "norm! zo"
+            end
+          end,
+        },
+      },
+      numbercolumn = {
+        numbercolumn = { padding = { right = 1 } },
+        condition = function() return require("astroui.status.condition").numbercolumn_enabled() end,
+        on_click = {
+          name = "line_click",
+          callback = function(...)
+            local args = require("astroui.status.utils").statuscolumn_clickargs(...)
+            if args.mods:find "c" then
+              local dap_avail, dap = pcall(require, "dap")
+              if dap_avail then dap.toggle_breakpoint() end
+            end
+          end,
+        },
+      },
+      signcolumn = {
+        signcolumn = {},
+        condition = function() return require("astroui.status.condition").signcolumn_enabled() end,
+        on_click = {
+          name = "sign_click",
+          callback = function(...)
+            local args = require("astroui.status.utils").statuscolumn_clickargs(...)
+            local config = assert(require("astroui").config.status)
+            if args.sign then
+              local handler = args.sign.name ~= "" and config.sign_handlers[args.sign.name]
+              if not handler then handler = config.sign_handlers[args.sign.namespace] end
+              if not handler then handler = config.sign_handlers[args.sign.texthl] end
+              if handler then handler(args) end
+            end
+          end,
+        },
+      },
+    },
     fallback_colors = {},
     icon_highlights = {},
     modes = {},
